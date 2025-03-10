e3c632a1525b27abd288efa0c16f8a7e54c878b3
Amit-committed-1 <AmitBhanj76@gmail.com>
2025-03-10 13:50:45 +0530

    Update A.java
    reference variable required inorder to access non static variable

--- src/main/java/in/psa/A.java
+++ src/main/java/in/psa/A.java
@@ -4,6 +4,6 @@ public class A {
     int x =1000;
     public static void main(String[] args){
         A a1 = new A();
-    System.out.println("value of x : "+x);
+    System.out.println("value of x : "+a1.x);
     }
 }
