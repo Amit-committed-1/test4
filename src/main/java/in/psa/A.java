6ea09255602a09376a43b7dd2fcb0e7aea0509e1
Amit-committed-1 <AmitBhanj76@gmail.com>
2025-03-10 13:47:16 +0530

    Update A.java
    added String[] args parameter in main method

--- src/main/java/in/psa/A.java
+++ src/main/java/in/psa/A.java
@@ -2,7 +2,7 @@ package in.psa;
 
 public class A {
     int x =1000;
-    public static void main(){
+    public static void main(String[] args){
     System.out.println("value of x : "+x);
     }
 }
